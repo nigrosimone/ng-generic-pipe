bbc989ea675d7aec537ab6d22a16eb43496ef2b0
Nigro Simone <nigro.simone@gmail.com>
2024-09-13 05:39:06 +0200

    chore: better test

--- projects/ng-generic-pipe-demo/src/app/app.component.ts
+++ projects/ng-generic-pipe-demo/src/app/app.component.ts
@@ -1,17 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, model, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgGenericPipe } from 'projects/ng-generic-pipe/src/public-api';
 import { Observable, of } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-root',
   template: `<main>
+    <input [(ngModel)]="arg1" > <input [(ngModel)]="arg2" ><br /><br />
   <!-- testFromPipe has a third parameter name for trigger pipe refresh -->
-  PIPE: {{ "arg1" | ngGenericPipe: testFromPipe:'arg2':name() }}<br /><br />
+  PIPE: {{ arg1() | ngGenericPipe: testFromPipe:arg2():name() }}<br /><br />
   <!-- wrong way for call a function into html just for test the result -->
-  HTML: {{ testFromHtml("arg1", "arg2") }}<br /><br />
+  HTML: {{ testFromHtml(arg1(), arg2()) }}<br /><br />
   <button (click)="triggerCD(false)">update</button>
   <button (click)="triggerCD(true)">force update</button>
   <br /><br />
@@ -19,11 +21,13 @@ import { Observable, of } from 'rxjs';
 </main>
 `,
   standalone: true,
-  imports: [NgGenericPipe, CommonModule]
+  imports: [NgGenericPipe, CommonModule, FormsModule]
 })
 export class AppComponent {
 
   public name = signal(0);
+  public arg1 = model('arg1');
+  public arg2 = model('arg2');
 
   constructor() {
     this.triggerCD(false);
