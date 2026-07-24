71a1f062ac57344b70e6ddc8b4fc7021ce9aeaf0
Nigro Simone <nigro.simone@gmail.com>
2026-07-24 06:27:48 +0200

    chore: better example

--- examples/stackblitz/src/app/app.component.ts
+++ examples/stackblitz/src/app/app.component.ts
@@ -1,22 +1,24 @@
-import { Component, signal } from '@angular/core';
+import { Component, model, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { NgGenericPipe } from 'ng-generic-pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [NgGenericPipe],
+  imports: [NgGenericPipe, FormsModule],
   template: `
+    <label for="name">Change name:</label>
+    <input type="text" id="name" [ngModel]="name()" (ngModelChange)="name.set($event)" style="width:50px" /> &nbsp;
+    <hr />
     <!-- calling greet(name()) directly would re-run on every change detection;
          through the pipe it is memoized, and this still points at the component -->
     <p>{{ name() | ngGenericPipe: greet }}</p>
-
-    <button (click)="name.set('Mario')">rename</button>
   `,
 })
 export class AppComponent {
-  readonly name = signal('Simone');
+  readonly name = model('Simone');
   private readonly greeting = 'Hello';
 
   greet(who: string): string {
-    return this.greeting + '! I am ' + who + '.';
+    return `${this.greeting}! I am ${who}.`;
   }
 }
